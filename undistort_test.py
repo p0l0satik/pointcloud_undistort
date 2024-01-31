ad02d8f626f53bf6dd42199757ac2b34066285ec
p0l0satik <denis.cucu@yandex.ru>
2024-01-31 22:11:31 +0300

    mr fixes

--- undistort_test.py
+++ undistort_test.py
@@ -8,11 +8,12 @@ from pathlib import Path
 from scipy.spatial.transform import Rotation as R
 from collections import defaultdict
 
-def get_intermediate_transform(T1, T2, t):
-    dxi = mrob.geometry.SE3(T2 @ np.linalg.inv(T1)).Ln()
-    return mrob.geometry.SE3(t * dxi).T() @ T1
 
-def undistort_segment(pcd, theta_min, theta_max, color, T):
+def get_intermediate_transform(initial_pose, target_pose, coef):
+    dxi = mrob.geometry.SE3(target_pose @ np.linalg.inv(initial_pose)).Ln()
+    return mrob.geometry.SE3(coef * dxi).T() @ initial_pose
+
+def undistort_segment(pcd, theta_min, theta_max, color, transform):
     # Convert the point cloud points to a numpy array
     points = np.asarray(pcd.points)
     thetas = np.asarray([math.atan2(p[1],p[0]) for p in points])
@@ -29,58 +30,66 @@ def undistort_segment(pcd, theta_min, theta_max, color, T):
         new_pcd.points = o3d.utility.Vector3dVector(new_points)
         new_pcd.colors = o3d.utility.Vector3dVector(new_colors)
 
-    return new_pcd.transform(T)
+    return new_pcd.transform(transform)
 
-def undistort_cloud(open3d_cloud, color, N, T1, T2):
-    N += 1 # starting separations from 0
+def undistort_cloud(cloud, start_angle, N_segments, initial_pose, target_pose):
+    N_segments += 1 # starting separations from 0
     undistort_result = []
-    prev_sector = -np.pi
-    coefs = np.linspace(0, 1, N)
-    sectors = np.linspace(-np.pi, np.pi, N)
+    coefs = np.linspace(0, 1, N_segments)
+    sectors = np.linspace(start_angle, start_angle + 2 * np.pi, N_segments)
+    color = [0.,0.,0.]
 
-    for i in range(1, N):
-        T = get_intermediate_transform(T1, T2, coefs[i])
+    for i in range(1, N_segments):
+        transform = get_intermediate_transform(initial_pose, target_pose, coefs[i])
         color[2] = coefs[i]
-        undistort_result.append(undistort_segment(open3d_cloud, prev_sector, sectors[i], color, np.asarray(T)))
-        prev_sector = sectors[i]
+        undistort_result.append(undistort_segment(cloud, start_angle, sectors[i], color, np.asarray(transform)))
+        start_angle = sectors[i]
     return undistort_result
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--dataset",
+        "--pcd_start",
         type=str,
-        default='pointcloud_undistort/dataset',
-        help="path to the pointclouds",
+        default='dataset/0.pcd',
+        help="pcd of initial pose",
+    )
+    parser.add_argument(
+        "--pcd_final",
+        type=str,
+        default='dataset/9.pcd',
+        help="pcd of final pose",
+    )
+    parser.add_argument(
+        "--N_segments",
+        type=int,
+        default=16,
+        help="number of segments to split pointcloud into",
     )
 
 
     args = parser.parse_args()
-    dataset = Path(args.dataset)
 
-    path0 = str(dataset/"0.pcd")
-    path9 = str(dataset/"9.pcd")
-
-    pcd_start = o3d.io.read_point_cloud(path0)
+    pcd_start = o3d.io.read_point_cloud(str(Path(args.pcd_start)))
     pcd_start = pcd_start.paint_uniform_color([1.0, 0.0, 0.0])
 
-    pcd_fin = o3d.io.read_point_cloud(path9)
+    pcd_fin = o3d.io.read_point_cloud(str(Path(args.pcd_final)))
     pcd_fin = pcd_fin.paint_uniform_color([0.0, 1.0, 0.0])
 
-    pcd_undistort = o3d.io.read_point_cloud(path9)
+    pcd_undistort = o3d.io.read_point_cloud(str(Path(args.pcd_final)))
     pcd_undistort = pcd_undistort.paint_uniform_color([0.0, 0.0, 0.0])
 
     
     pose_start = np.eye(4, 4)
-    pose_fin = np.asarray([
-        [ 0.99832152,  0.02603589,  0.05173264,  0.05743424],
-        [-0.02286862,  0.99788188, -0.0608998,  -0.04333022],
-        [-0.05320864,  0.05961452,  0.99680236, -0.01237881],
-        [ 0.0,         0.0,         0.0,         1.0     ]],
-        )
+    reg_p2p = o3d.pipelines.registration.registration_icp(
+        pcd_fin, pcd_start, 0.1, pose_start,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(),  
+        o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=2000),
+    )
 
-    N_patitions = 16
-    undistort_result = undistort_cloud(pcd_undistort, [0.0, 0.0, 0.0], N_patitions, pose_start, pose_fin)
+    start_angle = -np.pi
+    undistort_result = undistort_cloud(pcd_undistort, start_angle, args.N_segments, pose_start, reg_p2p.transformation)
+
     o3d.visualization.draw_geometries(undistort_result)
     o3d.visualization.draw_geometries([pcd_start,  pcd_fin] + undistort_result)
